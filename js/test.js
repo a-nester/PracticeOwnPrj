77dd616b4a65c92384d489fba15f3a1320a5cbff
a-nester <a_nester@ukr.net>
2024-01-28 17:50:12 +0200

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,23 @@
+function normalizeInput(input, to) {
+    if(to === "upper") {
+        return input.toUpperCase();
+    } else if (to === "lower") {
+        return input.toLowerCase();
+    } 
+}
+
+console.log(normalizeInput("This ISN'T SpaM", "lower"));
+console.log(normalizeInput("This ISN'T SpaM", "upper"));
+
+function checkForName(fullName, firstName) {
+  const lowerFullName = fullName.toLowerCase();
+  const lowerFirstName = firstName.toLowerCase();
+ return lowerFullName.includes(lowerFirstName);
+}
+
+console.log(checkForName("Caty Stars", "Caty"));
+console.log(checkForName("Jason Neis", "Jason"));
+console.log(checkForName("Jason Neis", "Jacob"));
+console.log(checkForName("Jason Neis", "jAsOn"));
+console.log(checkForName("Caty Stars", "cAtY"));
+
