50907389f55cf85f436493018b677f5cf8117b32
a-nester <a_nester@ukr.net>
2024-01-28 17:59:53 +0200

    +

--- js/test.js
+++ js/test.js
@@ -1,23 +1,33 @@
-function normalizeInput(input, to) {
-    if(to === "upper") {
-        return input.toUpperCase();
-    } else if (to === "lower") {
-        return input.toLowerCase();
-    } 
-}
+// function normalizeInput(input, to) {
+//     if(to === "upper") {
+//         return input.toUpperCase();
+//     } else if (to === "lower") {
+//         return input.toLowerCase();
+//     } 
+// }
 
-console.log(normalizeInput("This ISN'T SpaM", "lower"));
-console.log(normalizeInput("This ISN'T SpaM", "upper"));
+// console.log(normalizeInput("This ISN'T SpaM", "lower"));
+// console.log(normalizeInput("This ISN'T SpaM", "upper"));
 
-function checkForName(fullName, firstName) {
-  const lowerFullName = fullName.toLowerCase();
-  const lowerFirstName = firstName.toLowerCase();
- return lowerFullName.includes(lowerFirstName);
-}
+// function checkForName(fullName, firstName) {
+//   const lowerFullName = fullName.toLowerCase();
+//   const lowerFirstName = firstName.toLowerCase();
+//  return lowerFullName.includes(lowerFirstName);
+// }
 
-console.log(checkForName("Caty Stars", "Caty"));
-console.log(checkForName("Jason Neis", "Jason"));
-console.log(checkForName("Jason Neis", "Jacob"));
-console.log(checkForName("Jason Neis", "jAsOn"));
-console.log(checkForName("Caty Stars", "cAtY"));
+// console.log(checkForName("Caty Stars", "Caty"));
+// console.log(checkForName("Jason Neis", "Jason"));
+// console.log(checkForName("Jason Neis", "Jacob"));
+// console.log(checkForName("Jason Neis", "jAsOn"));
+// console.log(checkForName("Caty Stars", "cAtY"));
 
+function getFileName(file) {
+    const dotEnter = getFileName.indexOf(".");
+    if(dotEnter) {
+      return file.slice();
+    } else {
+      return file.slice(0, dotEnter);
+      }
+    }
+
+    console.log(getFileName("styles.css"));
